3653717a18eb7ce05e5d687bfd933029ebd32b47
Peu <peulins1234@gmail.com>
2025-11-02 19:37:36 -0300

    eliminando arquivo adicional

--- script.js
+++
@@ -1,8 +0,0 @@
-async function carregarDados() {
-  const response = await fetch("http://198.45.115.110:8080/api/v1/commands/")
-  if (!response.ok) throw new Error ("deu ruim")
-  const data = response.json()
-  console.log(data)
-}
-
-carregarDados()
